07afea23562a6f812861e3a08ca9da28fc06f826
nikhillingadhal1999 <nikhillingadhal1999@gmail.com>
2025-07-05 02:21:09 +0530

    commands and database client feature added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flexmetric",
-    version="0.1.0",
+    version="0.1.1",
     author="Nikhil Lingadhal",
     description="A flexible Prometheus exporter for commands, databases, functions, and scripts.",
     long_description=open("README.md").read(),
